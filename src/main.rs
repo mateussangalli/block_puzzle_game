636fde345df7c418d5f2d94bbdf524b863b36b14
Marina <marnie.s411@gmail.com>
2023-11-12 21:50:38 +0100

    successfully make a pair of pieces appear on screen

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@
 
 use bevy::prelude::*;
 
+const PIECE_SIZE: f32 = 32.;
+const RED: Color = Color::rgb(1., 0., 0.);
+const BLUE: Color = Color::rgb(0., 0., 1.);
+const GREEN: Color = Color::rgb(0., 1., 0.);
+const PURPLE: Color = Color::rgb(0.5, 0., 0.5);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, piece_movement)
         .run();
 }
 
@@ -18,20 +23,51 @@ enum Piece {
     Green,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        Piece::Red,
-        SpriteBundle {
-            texture: asset_server.load("sprites/red.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        }
-    ));
-}
+#[derive(Component)]
+struct FallingPair;
 
-fn piece_movement(time: Res<Time>, mut sprite_position: Query<(&mut Piece, &mut Transform)>) {
-    for (_, mut transform) in &mut sprite_position {
-        transform.translation.y -= 150. * time.delta_seconds();
-    }
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn((
+            FallingPair,
+            SpatialBundle {
+                transform: Transform::IDENTITY,
+                ..default()
+            }
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: RED,
+                        ..default()
+                    },
+                    transform: Transform {
+                        translation: Vec3::new(-PIECE_SIZE, 0., 0.),
+                        scale: Vec3::new(PIECE_SIZE, PIECE_SIZE, 1.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                Piece::Red,
+            ));
+
+            parent.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: PURPLE,
+                        ..default()
+                    },
+                    transform: Transform {
+                        translation: Vec3::new(0., 0., 0.),
+                        scale: Vec3::new(PIECE_SIZE, PIECE_SIZE, 1.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                Piece::Purple,
+            ));
+        });
 }
